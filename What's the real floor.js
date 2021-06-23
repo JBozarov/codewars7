6935117d37fa11967c3f1d38e5a5f039b0d40247
jbozarov <tech@jbozarov.com>
2021-06-22 20:19:33 -0400

    codewars (this) repo is emptied, all files moved to javascript-coding-challenges repo

--- What's the real floor.js
+++
@@ -1,3 +0,0 @@
-function getRealFloor(n) {
-  return n < 1 ? n : n > 13 ? n - 2 : n - 1; 
-}
--- findScreenHeight.js
+++
@@ -1,5 +0,0 @@
- function findScreenHeight(width, ratio) {
-   var arr = ratio.split(':')
-   var h = width/Number(arr[0])*Number(arr[1]); 
-   return `${width}x${h}`
-}
